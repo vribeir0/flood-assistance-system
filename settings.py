99006f24b12fb637c40edfe3e0488093bde7bb41
Vinicius Ribeiro <vncs_ribeiro@hotmail.com>
2025-05-27 21:06:29 -0300

    Adiciona envs do google maps

--- settings.py
+++ settings.py
@@ -12,22 +12,16 @@ BASE_DIR = pathlib.Path(__file__).parent.absolute()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL")
 
+# Google Maps API
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+GOOGLE_MAPS_API_URL = os.getenv("GOOGLE_MAPS_API_URL")
+
 # Geocoding API
 GEO_CODE_API_KEY = os.getenv("GEO_CODE_API_KEY")
-GEO_CODE_API_URL = os.getenv("GEO_CODE_API_URL", "https://geocode.maps.co/search")
+GEO_CODE_API_URL = os.getenv("GEO_CODE_API_URL")
 
 # Weather API
-WEATHER_API_URL = os.getenv("WEATHER_API_URL", "https://api.open-meteo.com/v1/forecast")
+WEATHER_API_URL = os.getenv("WEATHER_API_URL")
 
 # Server config
 SERVER_PATH = os.path.join(BASE_DIR, os.getenv("SERVER_PATH", "server.py"))
-
-# Databsase
-DATA_DIR = BASE_DIR / "data"
-DATABASE_URL = f"sqlite:///{DATA_DIR}/poc_mcp.db"
-
-if not GEMINI_API_KEY:
-    raise ValueError("A chave de API do Gemini não está configurada no arquivo .env")
-
-if not GEO_CODE_API_KEY:
-    raise ValueError("A chave de API de geocodificação não está configurada no arquivo .env")
